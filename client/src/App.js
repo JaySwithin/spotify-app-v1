6f93e5859c7734f850e5c9724f5435b024f51991
Swithin Okyere <jayswithin@gmail.com>
2022-06-01 17:55:40 +0000

    [UPDATE] - Done with playlists page

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
 import { Routes, Route } from "react-router-dom";
-import Playlist from "./pages/Playlist";
 import PlaylistDetails from "./pages/PlaylistDetails";
 import { GlobalStyle } from './styles';
-import { Login, Profile, TopArtists, TopTracks } from "./pages";
+import { Login, Profile, TopArtists, TopTracks, Playlists } from "./pages";
 import styled from 'styled-components/macro';
 
 
@@ -52,7 +51,7 @@ function App() {
               <Route path="/" element={<Profile />} />
               <Route path="/top-artists" element={<TopArtists />} />
               <Route path="/top-tracks" element={<TopTracks />} />
-              <Route path="/playlist" element={<Playlist />} />
+              <Route path="/playlists" element={<Playlists />} />
               <Route path="/playlist/:id" element={<PlaylistDetails />} />
             </Routes>
           </>
